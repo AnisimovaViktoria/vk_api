9e30399eaaaa9c0abd72b13855a85ce088868572
AnisimovaViktoria <50919440+AnisimovaViktoria@users.noreply.github.com>
2019-06-23 04:59:30 +0500

    initial commit

---
+++ vk_api/src/VkApi.java
@@ -0,0 +1,37 @@
+import json.JSONArray;
+import json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class VkApi {
+    public static void main(String[] args) throws IOException {
+
+        final String token = "e4038440e4038440e4038440efe468c28fee403e4038440b90d2a662f8942353e6902f9";
+        String userId = "98530885";
+        String method = "friends.get";
+        String stringUrl = "https://api.vk.com/method/" + method + "?user_id=" + userId + "&v=5.95&" + "access_token=" + token + "&order=name&" + "fields=last_name,first_name";
+
+        URL url = new URL(stringUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = "";
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        JSONObject jsonObject = new JSONObject(response.toString());
+        JSONObject obj = jsonObject.getJSONObject("response");
+        JSONArray arr =obj.getJSONArray("items");
+        for (int i=0;i<arr.length();i++)
+        {
+            String firstName=arr.getJSONObject(i).getString("first_name");
+            String lastName = arr.getJSONObject(i).getString("last_name");
+            System.out.println(firstName+" "+lastName);
+        }
+    }
+}
